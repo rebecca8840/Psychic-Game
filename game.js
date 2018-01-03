d5afc173bd67d2d4c4080f056c9a9a70b47d7654
rebecca8840 <31925939+rebecca8840@users.noreply.github.com>
2018-01-03 18:46:54 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // variables
 
-var computerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
-					"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 var wins = 0;
 
 var losses = 0;
@@ -16,32 +16,47 @@ var guessesSoFar = [];
 
 document.onkeyup = function(event) {
 
-	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-	var computerGuess = computerLetters[Math.floor(Math.random()*computerLetters.length)];
+    var computerGuess = computerLetters[Math.floor(Math.random()*computerLetters.length)];
 
-	guessesSoFar.push(userGuess);
-
-
-
-	if (userGuess == computerGuess) {
-
-				wins ++;
-				guessesLeft = 9;
-				guessesSoFar.length = 0;
-	}
+    guessesSoFar.push(userGuess);
 
 
-	else if (guessesLeft == 0) {
 
-			losses++;
-			guessesLeft = 9;
-			guessesSoFar.length = 0;
-	}
+    if (userGuess == computerGuess) {
 
- 	else if (userGuess !== computerGuess) {
+                wins ++;
+                guessesLeft = 9;
+                guessesSoFar.length = 0;
+    }
 
- 			guessesLeft--;
- 	}
 
+    else if (guessesLeft == 0) {
+
+            losses++;
+            guessesLeft = 9;
+            guessesSoFar.length = 0;
+    }
+
+    else if (userGuess !== computerGuess) {
+
+            guessesLeft--;
+    }
+
+
+    var html = "<h1>The Psychic Game</h1>" + 
+
+    "<p>Guess which letter I'm thinking of.</p>" + 
+
+    "<p> Wins: " +  wins + "</p>" + 
+
+    "<p> Losses: " + losses + "</p>" +
+
+    "<p>Guesses left: " + guessesLeft + "</p>" +
+
+    "<p>Your Guesses so far: " + guessesSoFar + "</p>";
+
+    document.querySelector(#game).innerHTML = html;
+    
 }
